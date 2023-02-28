053c98aa617bdd9413e2ace3e34617a1b1d6bf08
teste <teste@teste.com>
2023-02-28 18:19:16 -0300

    .

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$(uname -m)" == "arm64" ]; then
+    # Se for ARM64
+    echo "not supported arm64"
+    exit 1
+else
+    # Se for x64
+    URL="https://github.com/lucasvieceli/teste1111/releases/latest/download/bombcrypto-superbot-x64"
+fi
+
+
+ wget -O bot.zip "$URL"
